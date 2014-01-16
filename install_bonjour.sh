c588c4c84dafe43fee4068312164f062e5d5be15
Sjoerd Arendsen <sjoerd@optixdesigns.com>
2014-01-16 13:38:10 +0100

    fix paths

--- install_bonjour.sh
+++ install_bonjour.sh
@@ -8,8 +8,8 @@ sudo apt-get -qq update > /dev/null
 sudo apt-get -y -qq install libnss-mdns libavahi-compat-libdnssd1 > /dev/null
 sudo pip install pybonjour > /dev/null
 
-echo "Adding Screenly to autostart (via Supervisord)"
-sudo ln -sfT ~/screenly-manager/supervisor_bonjour.conf /etc/supervisor/conf.d/screenly_bonjour.conf
+echo "Adding Screenly Bonjour to autostart (via Supervisord)"
+sudo ln -sfT ~/screenly-manager/supervisor_bonjour.conf /etc/supervisor/conf.d/supervisor_bonjour.conf
 sudo /etc/init.d/supervisor stop > /dev/null
 sudo /etc/init.d/supervisor start > /dev/null
 
