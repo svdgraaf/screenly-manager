acec102f49a560decf6b70a897d836a2abe70c76
Sjoerd Arendsen <sjoerd@optixdesigns.com>
2014-01-16 13:28:48 +0100

    fix paths

--- install_bonjour.sh
+++ install_bonjour.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Downloading screenly-manager..."
-git clone git://github.com/svdgraaf/screenly-manager.git ~/screenly-manager > /dev/null
+git -q clone git://github.com/svdgraaf/screenly-manager.git ~/screenly-manager > /dev/null
 
 echo "Installing packages"
 sudo apt-get -qq update > /dev/null
@@ -12,3 +12,5 @@ echo "Adding Screenly to autostart (via Supervisord)"
 sudo ln -sfT ~/screenly-manager/supervisor_bonjour.conf /etc/supervisor/conf.d/screenly_bonjour.conf
 sudo /etc/init.d/supervisor stop > /dev/null
 sudo /etc/init.d/supervisor start > /dev/null
+
+echo "Done"
