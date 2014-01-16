48fafcd46616b3416559bacb6a91e55646553128
Sjoerd Arendsen <sjoerd@optixdesigns.com>
2014-01-16 13:15:04 +0100

    Force symlink

--- install_bonjour.sh
+++ install_bonjour.sh
@@ -3,14 +3,12 @@
 echo "Downloading screenly-manager..."
 git clone git://github.com/svdgraaf/screenly-manager.git ~/screenly-manager > /dev/null
 
-
 echo "Installing packages"
 sudo apt-get -qq update > /dev/null
 sudo apt-get -y -qq install libnss-mdns libavahi-compat-libdnssd1 > /dev/null
 sudo pip install pybonjour > /dev/null
 
-
 echo "Adding Screenly to autostart (via Supervisord)"
-sudo ln -s ~/screenly-manager/supervisor_bonjour.conf /etc/supervisor/conf.d/screenly_bonjour.conf
+sudo ln -sfT ~/screenly-manager/supervisor_bonjour.conf /etc/supervisor/conf.d/screenly_bonjour.conf
 sudo /etc/init.d/supervisor stop > /dev/null
 sudo /etc/init.d/supervisor start > /dev/null
